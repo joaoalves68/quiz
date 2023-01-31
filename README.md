cd48dc6174f23763c265cd0f47343431e9954373
joao <joao-alvespa@hotmail.com>
2023-01-31 16:36:26 -0300

    'finalizado'

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 ### Executar o arquivo no cmd:
 #####   - py quiz.py
+
+### Adiciconar perguntas no excell da seguinte forma:
+#####   - coluna "QUESTIONS" adicionar a pergunta e em seguida as alternativas separadas por "||".
+#####   ex: Qual a operação contrária a potenciação? || a) Divisão || b) Radiciação || c) Adição || d) Multiplicação
+#####   - coluna "ANSWERS" adicionar a letra da alternativa correta.
--- quiz.py
+++ quiz.py
@@ -1,57 +1,32 @@
+import pandas as pd
+
 def checarResposta (resposta, resposta_usuario):
     if resposta == resposta_usuario:
         return True
 
 def calcularScore (qntPerguntas, qntAcertos):
-    return qntAcertos / qntPerguntas * 100
+    return int(qntAcertos) / int(qntPerguntas) * 100
 
 print('Olá, bem-vindo ao quiz do João')
-comecar = input('Deseja começar o quiz? (S/N) ')
 
-if comecar != 'S':
+comecar = input('Deseja começar o quiz? (S/N) ').lower()  
+if comecar != 's':
+    print('\nOk, até a próxima!')
     quit()
 
 score = 0
 
-pergunta_1 = "\n1: Qual a operação contrária a potenciação? \n a) Divisão \n b) Radiciação \n c) Adição \n d) Multiplicação "
-resposta_1 = "b"
+questions = pd.read_excel(r'C:\Users\JOAO\Desktop\quiz\questions.xlsx')
 
-pergunta_2 = "\n2: O Brasil foi colônia de qual país? \n a) Portugal \n b) Itália \n c) França \n d) Inglaterra "
-resposta_2 = "a"
+quantidadePerguntas = int(input('Quantas perguntas deseja responder? (MAX: '+str(questions['QUESTIONS'].count())+') '))
+if quantidadePerguntas > questions['QUESTIONS'].count():
+    quit()
 
-pergunta_3 = "\n3: Qual físico produziu três leis que levaram seu nome na sua denominação? \n a) Einsten \n b) Newton \n c) Karl Marx \n d) Rosseau "
-resposta_3 = "b"
+for x in range(quantidadePerguntas):
+    print('\n'+questions['QUESTIONS'][x].replace("||", "\n"))
+    respostaUsuario = input("Resposta: ").lower()
+    if checarResposta(questions['ANSWERS'][x], respostaUsuario) == True:
+        score += 1
 
-pergunta_4 = "\n4: Qual destes países não se localiza na América do sul? \n a) Angola \n b) Bolívia \n c) Suriname \n d) Guiana "
-resposta_4 = "a"
-
-pergunta_5 = "\n5: 'Comer' é um: \n a) Artigo \n b) Adjetivo \n c) Verbo \n d) Pronome "
-resposta_5 = "c"
-
-print(pergunta_1)
-resposta_usuario = input("Resposta: ")
-if checarResposta(resposta_1, resposta_usuario) == True:
-    score += 1
-
-print(pergunta_2)
-resposta_usuario = input("Resposta: ")
-if checarResposta(resposta_2, resposta_usuario) == True:
-    score += 1
-
-print(pergunta_3)
-resposta_usuario = input("Resposta: ")
-if checarResposta(resposta_3, resposta_usuario) == True:
-    score += 1
-
-print(pergunta_4)
-resposta_usuario = input("Resposta: ")
-if checarResposta(resposta_4, resposta_usuario) == True:
-    score += 1
-
-print(pergunta_5)
-resposta_usuario = input("Resposta: ")
-if checarResposta(resposta_5, resposta_usuario) == True:
-    score += 1
-
-scoreFinal = calcularScore(5, score)
-print('Quiz finalizado, você acertou '+str(scoreFinal)+'%, parabéns!')
+scoreFinal = calcularScore(quantidadePerguntas, score)
+print('\n\nQuiz finalizado, você acertou '+str(scoreFinal)+'%, parabéns!')
